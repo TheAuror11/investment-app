811b43cb5097e9afb05f55cd49c416f8e5cdedc3
Tejesh Verma <tejeshver1@outlook.com>
2024-08-23 01:43:24 +0530

    Final

--- utils/otp.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-// Generate a 4-digit OTP
-const generateOTP = () => {
-  return crypto.randomInt(1000, 9999).toString();
-};
-
-module.exports = { generateOTP };
