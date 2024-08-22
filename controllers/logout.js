bc0545a100ce8a7d7b52a0ec4130f2efd6112977
Tejesh Verma <tejeshver1@outlook.com>
2024-08-23 02:08:56 +0530

    Final

--- controllers/logout.js
+++ controllers/logout.js
@@ -3,6 +3,9 @@ const jwt = require("jsonwebtoken");
 // @desc    Logout user
 // @route   POST /api/auth/logout
 const logoutUser = async (req, res) => {
+  req.headers["authorization"] = undefined;
+  console.log(req.headers["authorization"]);
+
   return res.clearCookie("token").json({ message: "User Logged Out" });
 };
 
